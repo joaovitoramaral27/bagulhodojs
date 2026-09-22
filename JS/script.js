2a8df86b383a89b2b4c28d3429145ca3e2e97af7
joaovitoramaral27 <otthersfunec@gmail.com>
2026-09-22 16:36:24 -0300

    BUG NO JS MAS O CODIGO TA BUNITIM KSKSKS

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,28 @@
+caixa1 = document.querySelector("#1");
+caixa2 = document.querySelector("#2");
+caixa3 = document.querySelector("#3");
 btn1 = document.querySelector(".btn1");
-btn1.addEventListener("click", function() {
-    btn1.style.border = "5px solid white";
-    if (btn1.style.border === "5px solid white") {
-        btn1.addEventListener("click", function() {
-            btn1.style.border = "none";
-        });
-    }
-    else {
-        btn1.style.border = "5px solid white";
-    }
+btn2 = document.querySelector(".btn2");
+
+caixa1.addEventListener("click", function() {
+    caixa1.style.border = "5px solid white";
+    print("Você clicou na caixa 1");
 });
 
-btn2 = document.querySelector(".btn2");
+caixa2.addEventListener("click", function() {
+    caixa2.style.border = "5px solid white";
+    print("Você clicou na caixa 2");
+});
+
+caixa3.addEventListener("click", function() {
+    caixa3.style.border = "5px solid white";
+    print("Você clicou na caixa 3");
+});
+    
+btn1.addEventListener("click", function() {
+    caixa1.style.border = "5px solid red";
+});
+
 btn2.addEventListener("click", function() {
-    btn2.style.border = "5px solid white";
-    if (btn2.style.border === "5px solid white") {
-        btn2.addEventListener("click", function() {
-            btn2.style.border = "none";
-        });
-    }
-    else {
-        btn1.style.border = "5px solid white";
-    }
+    caixa1.style.border = "none";
 });
